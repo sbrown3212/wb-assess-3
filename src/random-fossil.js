e79508ea08e0e291a65a755680492c0254e3ec21
Stephen Brown <stephen.jr36@gmail.com>
2024-08-16 11:59:13 -0600

    completed: displaying a random fossil

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,1 +1,16 @@
+import axios from "axios";
+
 console.log('Hello World');
+
+document.querySelector('#get-random-fossil').addEventListener('click', (e) => {
+    axios.get('/random-fossil.json').then((response) => {
+        // Deconstruct response data
+        const { img, name } = response.data;
+
+        // // Create img tag string
+        // const imgTag = `<img src="${img}">`
+
+        document.querySelector('#random-fossil-image').innerHTML = `<img src="${img}">`;
+        document.querySelector('#random-fossil-name').innerText = name;
+    })
+})
